894e49bb9fcc86339d0e65fba5d4abbcfd2d7f1d
tookhy <92835776+Altookhy@users.noreply.github.com>
2023-07-19 01:15:18 +0800

    add longuitude

--- models/Disease.js
+++ models/Disease.js
@@ -1,3 +1,26 @@
+// module.exports = (sequelize, DataTypes) => {
+//     const Disease = sequelize.define("Disease", {
+//         country: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         disease: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         vaccine: {
+//             type: DataTypes.STRING,
+//             allowNull: true
+//         },
+//         cases: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false
+//         }
+//     });
+//     return Disease;
+// };
+ 
+
 module.exports = (sequelize, DataTypes) => {
     const Disease = sequelize.define("Disease", {
         country: {
@@ -15,8 +38,15 @@ module.exports = (sequelize, DataTypes) => {
         cases: {
             type: DataTypes.INTEGER,
             allowNull: false
+        },
+        longitude: {
+            type: DataTypes.FLOAT, // Adjust the data type based on your requirements
+            allowNull: true
+        },
+        magnitude: {
+            type: DataTypes.FLOAT, // Adjust the data type based on your requirements
+            allowNull: true
         }
     });
     return Disease;
 };
- 
